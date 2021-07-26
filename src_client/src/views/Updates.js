ed8bf0be01131167ce0f8988837501c4b367a241
Bon Nicolai Mercado <bon_hokage06@yahoo.com>
2021-07-26 15:07:16 +0800

    add installed version display in updates page

--- src_client/src/views/Updates.js
+++ src_client/src/views/Updates.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import {Row,Col,Card,CardHeader} from "reactstrap"
-export default function Updates({isMobile,ota}) {
-    const {currentVersionDetails}=ota
-    return (
-        <div        
+import React from "react"
+import { Row, Col, Card, CardHeader, CardBody } from "reactstrap"
+export default function Updates({ isMobile, ota }) {
+  const { currentVersionDetails } = ota
+  console.log(currentVersionDetails)
+  return (
+    <div
+      style={{
+        ...{
+          paddingLeft: "18%",
+          width: "100%",
+          backgroundColor: "#1E1E26",
+        },
+        ...(isMobile && { paddingLeft: undefined }),
+      }}
+      className="animated fadeIn w3-container"
+    >
+      <Row>
+        <Col>
+          <Card
             style={{
-            ...{
-              paddingLeft: "18%",
-              width: "100%",
-              backgroundColor: "#1E1E26",
-            },
-            ...(isMobile && { paddingLeft: undefined }),
-          }}
-          className="animated fadeIn w3-container">
-            <Row>
-              <Col>
-                <Card
-                  style={{
-                    backgroundColor: "#272A3D",
-                    color: "white",
-                    fontSize: "16px",
-                    marginBottom: "20px",
-                  }}
-                >
-                  <CardHeader>Control your Elabox</CardHeader>
-                </Card>
-              </Col>
-          </Row>            
-        </div>
-    )
+              backgroundColor: "#272A3D",
+              color: "white",
+              fontSize: "16px",
+              marginBottom: "20px",
+            }}
+          >
+            <CardHeader>Installed Version</CardHeader>
+            <CardBody>
+              <p>Name: {currentVersionDetails.name}</p>
+              <p>Version: {currentVersionDetails.version}</p>
+              <p>{currentVersionDetails.description}</p>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  )
 }
