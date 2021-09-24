1683e91dcd6e338c2cef6ee5cb48e9dfa02fc988
Bon Nicolai Mercado <bonhokage06@gmail.com>
2021-09-24 14:42:14 +0000

    add margin on widget05

--- src_client/src/views/widgets/Widget05.js
+++ src_client/src/views/widgets/Widget05.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import { mapToCssModules } from 'reactstrap/lib/utils';
-import { Button, Row, Col, Card, CardBody } from 'reactstrap';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import classNames from "classnames";
+import { mapToCssModules } from "reactstrap/lib/utils";
+import { Button, Row, Col, Card, CardBody } from "reactstrap";
+import PropTypes from "prop-types";
 
 const propTypes = {
   children: PropTypes.node,
@@ -12,68 +12,90 @@ const propTypes = {
 };
 
 const defaultProps = {
-  dataBox: () => ({ variant: 'facebook', friends: '-', feeds: '-' }),
-  disabledButton:false
+  dataBox: () => ({ variant: "facebook", friends: "-", feeds: "-" }),
+  disabledButton: false,
 };
 
 class Widget05 extends Component {
-
   // constructor(props){
   //   super(props)
   // }
 
   render() {
-
     // eslint-disable-next-line
-    const { children, className, cssModule, dataBox, ...attributes } = this.props;
+    const { children, className, cssModule, dataBox, ...attributes } =
+      this.props;
 
     // demo purposes only
     const data = dataBox();
     const variant = data.variant;
     const title = data.title;
-    if (!variant || ['facebook', 'twitter', 'linkedin', 'google-plus'].indexOf(variant) < 0) {
-      return (null);
+    if (
+      !variant ||
+      ["facebook", "twitter", "linkedin", "google-plus"].indexOf(variant) < 0
+    ) {
+      return null;
     }
 
-    const back = 'bg-' + variant;
-    const icon = 'fa fa-' + variant;
+    const back = "bg-" + variant;
+    const icon = "fa fa-" + variant;
     const keys = Object.keys(data);
     const vals = Object.values(data);
 
-    const classCard = 'brand-card';
+    const classCard = "brand-card";
     const classCardHeader = classNames(`${classCard}-header`, back);
     const classCardBody = classNames(`${classCard}-body`);
-    const classes = mapToCssModules(classNames(classCard, className), cssModule);
+    const classes = mapToCssModules(
+      classNames(classCard, className),
+      cssModule
+    );
 
     return (
-          <Card style={{backgroundColor:'#272A3D', color:'white'}}>
-              <CardBody >
-                <Row style={{height: '70px'}}>
-                    <Col style={{display: 'flex', justifyContent: 'center'}}>
-                    {data.title}
-                    </Col>
-                    
-                </Row>
+      <Card
+        style={{
+          backgroundColor: "#272A3D",
+          color: "white",
+          margin: "0.5em 0 0.5em 0",
+        }}
+      >
+        <CardBody>
+          <Row style={{ height: "70px" }}>
+            <Col style={{ display: "flex", justifyContent: "center" }}>
+              {data.title}
+            </Col>
+          </Row>
 
-                {vals[3] != "" ?
-                    <Row>   
-                        <Col xs="6" style={{justifyContent:'center', display:'flex' }}>
-                          <Button onClick={this.props.onGreenPress} color="success">{vals[2]}</Button>
-                        </Col>
-                        <Col xs="6" style={{justifyContent:'center', display:'flex' }}>
-                            <Button onClick={this.props.onRedPress} color="danger">{vals[3]}</Button>
-                        </Col>
-                    </Row>
-                :
-                <Row>   
-                        <Col xs="12" style={{justifyContent:'center', display:'flex' }}>
-                            <Button disabled={this.props.disabledButton} onClick={this.props.onGreenPress} color="success">{vals[2]}</Button>
-                        </Col>
-                    </Row>                  
-                }
-            </CardBody>
-          </Card>
-
+          {vals[3] != "" ? (
+            <Row>
+              <Col xs="6" style={{ justifyContent: "center", display: "flex" }}>
+                <Button onClick={this.props.onGreenPress} color="success">
+                  {vals[2]}
+                </Button>
+              </Col>
+              <Col xs="6" style={{ justifyContent: "center", display: "flex" }}>
+                <Button onClick={this.props.onRedPress} color="danger">
+                  {vals[3]}
+                </Button>
+              </Col>
+            </Row>
+          ) : (
+            <Row>
+              <Col
+                xs="12"
+                style={{ justifyContent: "center", display: "flex" }}
+              >
+                <Button
+                  disabled={this.props.disabledButton}
+                  onClick={this.props.onGreenPress}
+                  color="success"
+                >
+                  {vals[2]}
+                </Button>
+              </Col>
+            </Row>
+          )}
+        </CardBody>
+      </Card>
     );
   }
 }
