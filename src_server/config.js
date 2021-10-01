8681d0a158b9905e11ead6b81152a58dbfcf07a3
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2021-10-01 01:51:23 +0000

    updated comments

--- src_server/config.js
+++ src_server/config.js
@@ -24,32 +24,32 @@ module.exports = {
   SUPERNODE: homeapps,
   ESC_DIR: homeapps + "/ela.esc",
   EID_DIR: homeapps + "/ela.eid",
-  ELA_DIR: elaPath, //
-  ELADATA_DIR: eladatadir, // where keystore will be saved
-  ELABLOCKS_DIR: eladatadir + "/elastos", // mainchain blocks will be save
-  EIDDATA_DIR: homeappdata + "/ela.eid", // eid data directory
+  ELA_DIR: elaPath,                                   // ela mainchain bin path
+  ELADATA_DIR: eladatadir,                            // where keystore will be saved
+  ELABLOCKS_DIR: eladatadir + "/elastos",             // mainchain blocks will be save
+  EIDDATA_DIR: homeappdata + "/ela.eid",              // eid data directory
   ESCDATA_DIR: homeappdata + "/ela.esc",
-  EID_PORT: 20645, // eid port that can be access for web3 api
-  ESC_PORT: 20646,
+  EID_PORT: 20645,                                    // eid port that can be access for web3 api
+  ESC_PORT: 20646,                                    // esc port that can be access for web3 api
   ELA_PORT: 20336,
-  CARRIER_DIR: homeapps + "/ela.carrier", // carrier app directory
-  KEYSTORE_PATH: eladatadir + "/keystore.dat", // keystore data path
+  CARRIER_DIR: homeapps + "/ela.carrier",             // carrier app directory
+  KEYSTORE_PATH: eladatadir + "/keystore.dat",        // keystore data path
   SUPPORT_EMAIL: "purujit.bansal9@gmail.com",
   STORAGE_PATH: storagePath,
-  TMP_PATH: tmpPath, // where files will be temporary save. specifically use for installer
-  ELA_SYSTEM_PATH: elaSystemPath, // dir where the system apps installed
-  ELA_SYSTEM_INFO_PATH: elaSystemInfoPath, // where information about the system installed
-  ELA_SYSTEM_INSTALLER_PATH: elaSystemInstallerPath, // dir for installer binary
-  ELA_SYSTEM_TMP_PATH: elaTmpPath, // temp/cache path
-  ELA_SYSTEM_TMP_INSTALLER: elaInstaller, // the path where temp installer will be copied during system update
+  TMP_PATH: tmpPath,                                  // where files will be temporary save. specifically use for installer
+  ELA_SYSTEM_PATH: elaSystemPath,                     // dir where the system apps installed
+  ELA_SYSTEM_INFO_PATH: elaSystemInfoPath,            // where information about the system installed
+  ELA_SYSTEM_INSTALLER_PATH: elaSystemInstallerPath,  // dir for installer binary
+  ELA_SYSTEM_TMP_PATH: elaTmpPath,                    // temp/cache path
+  ELA_SYSTEM_TMP_INSTALLER: elaInstaller,             // the path where temp installer will be copied during system update
   ELA_SYSTEM: "ela.system",
   ELA_EID: "ela.eid",
-  ELA_EID_UPDATE_ACTION: "ela.eid.action.UPDATE",
-  ELA_SYSTEM_BROADCAST: "ela.system.BROADCAST",
+  ELA_EID_UPDATE_ACTION: "ela.eid.action.UPDATE",     // action id for updating system
+  ELA_SYSTEM_BROADCAST: "ela.system.BROADCAST",       // id for broadcasting message to system
   INSTALLER_PK_ID: "ela.installer",
-  ELA_SYSTEM_BROADCAST_ID_INSTALLER: "ela.installer.PROGRESS",
+  ELA_SYSTEM_BROADCAST_ID_INSTALLER: "ela.installer.PROGRESS", // installer action id that tells the companion front end it is in progress
   BUILD_MODE: buildMode,
-  ELABOX_VERSION: elaboxVersion, // current version of elabox
+  ELABOX_VERSION: elaboxVersion,                      // current version of elabox
   PACKAGES_URL:
     buildMode === "RELEASE"
       ? "https://storage.googleapis.com/elabox/packages"
