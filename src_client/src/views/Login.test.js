3917b56eb8c0ce266becd466949a950f14b49f88
Bon Nicolai Mercado <bonhokage06@gmail.com>
2021-10-08 18:14:51 +0000

    remove act into dashboard and login test

--- src_client/src/views/Login.test.js
+++ src_client/src/views/Login.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, screen, act,waitFor } from "@testing-library/react"
+import { fireEvent, screen,waitFor } from "@testing-library/react"
 import { server, rest, BASE_URL, renderApp,clearLocalStorage } from "../testing/utils"
 
 beforeAll(() => server.listen())
@@ -6,38 +6,33 @@ afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 describe("Login",()=>{
     test("Able to login",async ()=>{
-        await act(async ()=>{
-            clearLocalStorage()
-            renderApp()
-            await screen.findByText("Sign In")
-            const passwordInput = screen.getByTestId("password")
-            const signInBtn = screen.getByTestId("sign-in-btn")
-            fireEvent.change(passwordInput, { target: { value: "Tester" } })
-            fireEvent.click(signInBtn)                
-            const dashboardElement = await screen.findByText("Dashboard")
-            expect(dashboardElement).toBeInTheDocument()                    
-        })
+        clearLocalStorage()
+        renderApp()
+        await screen.findByText("Sign In")
+        const passwordInput = screen.getByTestId("password")
+        const signInBtn = screen.getByTestId("sign-in-btn")
+        fireEvent.change(passwordInput, { target: { value: "Tester" } })
+        fireEvent.click(signInBtn)                
+        const dashboardElement = await screen.findByText("Dashboard")
+        expect(dashboardElement).toBeInTheDocument()                    
     })
     test("Not able to login",async ()=>{
-        await act(async ()=>{
-            server.use(rest.post(`${BASE_URL}/login`, (req, res, ctx) => {
-                return res(
-                  ctx.json({
-                    ok: false,
-                  })
-                )
-            }))
-            clearLocalStorage()
-            jest.spyOn(window, 'alert').mockImplementation(() => {});    
-            renderApp()
-            await screen.findByText("Sign In")
-            const passwordInput = screen.getByTestId("password")
-            const signInBtn = screen.getByTestId("sign-in-btn")
-            fireEvent.change(passwordInput, { target: { value: "Tester" } })
-            fireEvent.click(signInBtn)                    
-            await waitFor(()=> expect(window.alert).toHaveBeenCalledWith("Wrong password"))
-            expect(window.alert).toHaveBeenCalled()
-
-        })
+        server.use(rest.post(`${BASE_URL}/login`, (req, res, ctx) => {
+            return res(
+              ctx.json({
+                ok: false,
+              })
+            )
+        }))
+        clearLocalStorage()
+        jest.spyOn(window, 'alert').mockImplementation(() => {});    
+        renderApp()
+        await screen.findByText("Sign In")
+        const passwordInput = screen.getByTestId("password")
+        const signInBtn = screen.getByTestId("sign-in-btn")
+        fireEvent.change(passwordInput, { target: { value: "Tester" } })
+        fireEvent.click(signInBtn)                    
+        await waitFor(()=> expect(window.alert).toHaveBeenCalledWith("Wrong password"))
+        expect(window.alert).toHaveBeenCalled()
     })        
 })
