aeea97ee85bc3df09fb51ac2c029ca305a35fe5c
Bon Nicolai Mercado <bonhokage06@gmail.com>
2021-10-08 13:27:25 +0000

    update login and help center test

--- src_client/src/views/Login.test.js
+++ src_client/src/views/Login.test.js
@@ -1,19 +1,19 @@
 import { fireEvent, screen, act,waitFor } from "@testing-library/react"
-import { server, rest, BASE_URL, renderApp,clearLocalStorage } from "../setupTests"
+import { server, rest, BASE_URL, renderApp,clearLocalStorage } from "../testing/utils"
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 describe("Login",()=>{
-    beforeEach(() =>  clearLocalStorage())    
     test("Able to login",async ()=>{
         await act(async ()=>{
+            clearLocalStorage()
             renderApp()
             await screen.findByText("Sign In")
             const passwordInput = screen.getByTestId("password")
             const signInBtn = screen.getByTestId("sign-in-btn")
             fireEvent.change(passwordInput, { target: { value: "Tester" } })
-            fireEvent.click(signInBtn)
+            fireEvent.click(signInBtn)                
             const dashboardElement = await screen.findByText("Dashboard")
             expect(dashboardElement).toBeInTheDocument()                    
         })
@@ -27,13 +27,14 @@ describe("Login",()=>{
                   })
                 )
             }))
-            jest.spyOn(window, 'alert').mockImplementation(() => {});
+            clearLocalStorage()
+            jest.spyOn(window, 'alert').mockImplementation(() => {});    
             renderApp()
             await screen.findByText("Sign In")
             const passwordInput = screen.getByTestId("password")
             const signInBtn = screen.getByTestId("sign-in-btn")
             fireEvent.change(passwordInput, { target: { value: "Tester" } })
-            fireEvent.click(signInBtn)     
+            fireEvent.click(signInBtn)                    
             await waitFor(()=> expect(window.alert).toHaveBeenCalledWith("Wrong password"))
             expect(window.alert).toHaveBeenCalled()
 
