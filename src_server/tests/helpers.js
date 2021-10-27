4349d30f5a56fd0a18bae52ad4176c58f9e96fd7
Bon Nicolai Mercado <bonhokage06@gmail.com>
2021-10-27 11:57:02 +0000

    create test helper file

---
+++ src_server/tests/helpers.js
@@ -0,0 +1,14 @@
+const validateIPaddress = (ipaddress)=> {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
+      return (true)  
+    }  
+    return (false)  
+  }  
+  const validateEmail = (email)=>{
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());    
+  } 
+  module.export={
+    validateIPaddress,
+    validateEmail
+  }
