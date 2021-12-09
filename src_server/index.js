5a6fc348b62b097adbf96ba7bc24a66e038660a7
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2021-12-09 07:45:30 +0000

    bug fixes

--- src_server/index.js
+++ src_server/index.js
@@ -666,7 +666,6 @@ app.use("/", router)
 const startServer=()=>{
   app.listen(config.PORT, async function () {
     syslog.write(syslog.create().info("Companion start running on " + config.PORT))
-    return
     checkProcessingRunning("ela-bootstrapd").then((running) => {
       if (!running) restartCarrier((response) => {
         syslog.write(syslog.create().debug('Carrier restart response ' + response))
