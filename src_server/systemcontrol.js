83407751037c926a34b6ab7090d55708f49c97ce
Firiyuu <stormmuhokushikiro@yahoo.com>
2022-01-27 07:35:58 +0000

    add import keystore functionaloty

--- src_server/systemcontrol.js
+++ src_server/systemcontrol.js
@@ -15,8 +15,30 @@ let keyStorePath = config.KEYSTORE_PATH;
 
 // create a routes folder and add routes there
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
 
+// Mutler for storage of keystore dat file
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, config.ELADATA_DIR)
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'keystore.dat')
+  }
+})
+
+var uploads = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == "application/octet-stream") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error('Only .dat format allowed!'));
+    }
+  }
+});
 
 router.post("/shutdown", (req, res) => {
   syslog.create().info("SHUTDOWN COMMAND RECIEVED").addCategory("system")
@@ -35,6 +57,18 @@ router.post("/restart", (req, res) => {
 
   );
 });
+
+
+router.post('/import-keystore', uploads.single('keystore'), function(req, res, next) {
+  syslog.create().info("Imported new keystore.").addCategory("system")
+  console.log("Imported new keystore")
+  console.log(req.file);
+  return res.json({success: true});
+
+
+  //...
+});
+
 
 
 module.exports = router;
