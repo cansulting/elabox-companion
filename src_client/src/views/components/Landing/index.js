8de1b382206cf4db80d92f8da3281e4df7af4e83
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-02-14 08:50:46 +0000

    updated landing

--- src_client/src/views/components/Landing/index.js
+++ src_client/src/views/components/Landing/index.js
@@ -3,8 +3,7 @@ import ElaboxLandingLogo from "../../images/logo-landing.png"
 export default function Landing() {
   return (
     <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",width:"100vw",height:"100vh",backgroundColor:"rgb(39, 42, 61)"}}>
-        <img src={ElaboxLandingLogo}/>
-        <p style={{color: "white",fontSize:"1.6em",marginTop:"0.4em" }}> Please Wait </p>        
+        <img src={ElaboxLandingLogo} width="200px"/>     
     </div>
   )
 }
