0492c6018ee982f7a85dab93401f5b0319dbda04
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-02-16 15:13:18 +0000

    update copy message

--- src_client/src/views/Dashboard.js
+++ src_client/src/views/Dashboard.js
@@ -154,16 +154,16 @@ const Dashboard = ({ isMobile }) => {
             <center>
             <img src={didLogo} style={{ width: "50px", height: "50px",marginBottom: 5 }} />              
             <p style={{fontSize: 20}}>Hostname: {esc?.hostname}<CopyToClipboard text={esc?.hostname}
-                onCopy={() =>{alert("hostname copied to clipboard")}}>
+                onCopy={() =>{alert("Hostname copied to clipboard")}}>
                   <Button color="success" style={{marginLeft:3,padding:5}} size="sm"><AiFillCopy/></Button>
               </CopyToClipboard>              
               </p>
               <p style={{fontSize: 20}}>Port: {esc?.port} <CopyToClipboard text={esc?.port}
-                onCopy={() =>{alert("port copied to clipboard")}}>
+                onCopy={() =>{alert("Port copied to clipboard")}}>
                   <Button color="success" style={{marginLeft:3,padding:5}} size="sm"><AiFillCopy/></Button>
               </CopyToClipboard></p>              
               <p style={{fontSize: 20}}>Chain Id: {esc?.chainId} <CopyToClipboard text={esc?.chainId}
-                onCopy={() =>{alert("port copied to clipboard")}}>
+                onCopy={() =>{alert("Chain id copied to clipboard")}}>
                   <Button color="success" style={{marginLeft:3,padding:5}} size="sm"><AiFillCopy/></Button>
               </CopyToClipboard></p>                            
             </center>
