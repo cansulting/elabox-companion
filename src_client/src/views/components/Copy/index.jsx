d168730f61192c9d45c1a71ac464f9111e45f2f6
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-02-23 06:37:15 +0000

    updated closing tooltip

--- src_client/src/views/components/Copy/index.jsx
+++ src_client/src/views/components/Copy/index.jsx
@@ -3,23 +3,26 @@ import { Button,Tooltip } from "reactstrap"
 import copy from "copy-to-clipboard";
 import {AiFillCopy} from "react-icons/ai"
 export default function Copy({id,label,data}) {
-  const [isToolTipOpen,setIsToolTipOpen]=useState(false)    
+  const [isToolTipOpen,setIsToolTipOpen]=useState(false)  
+  const handleCopy=()=>{
+    setIsToolTipOpen(true)
+    copy(data)
+    setTimeout(()=>{
+      setIsToolTipOpen(false)
+    },2000)
+  }  
   return (
       <>
           <Tooltip
-            autohide
             isOpen={isToolTipOpen}
-            placement="right"
+            placement="top"
             target={id}
             trigger="click"
             >
               Copied
             </Tooltip>                  
-            <p style={{fontSize: 17}} onMouseLeave={()=>{
-                setIsToolTipOpen(false)
-            }}>{label}: {data} <Button id={id} color="success" style={{marginLeft:3,padding:5,background:"transparent",border:"none"}} size="sm" onClick={()=>{
-                setIsToolTipOpen(true)
-                copy(data)
+            <p style={{fontSize: 17}}>{label}: {data} <Button id={id} color="success" style={{marginLeft:3,padding:5,background:"transparent",border:"none"}} size="sm" onClick={()=>{
+                handleCopy()
             }}><AiFillCopy/></Button> 
       </p>            
       </>
