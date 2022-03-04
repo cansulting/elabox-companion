16747736c4c4af51950447d8f9b647cf38b123da
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-03-04 13:00:44 +0000

    add activation comparion page

--- src_client/src/views/Activation.js
+++ src_client/src/views/Activation.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import { Row, Col, Button, Container } from "reactstrap"
+import React, { useState } from 'react'
+import { Link, useHistory } from "react-router-dom"
+import * as Icon from "react-feather"
+import { Button, Table } from "reactstrap"
 export default function Activation({ isMobile }) {
+    const [currentPage, setCurrentPage] = useState(0)
+    const history = useHistory();
+    const handleNextPage = () => {
+        setCurrentPage(currentPage + 1)
+    }
+    const handlePrevPage = (path = "") => {
+        setCurrentPage(currentPage - 1)
+        if (path?.length > 0) {
+            history.push(path)
+        }
+    }
     return (
         <div style={{
             ...{
@@ -16,14 +29,68 @@ export default function Activation({ isMobile }) {
             },
             ...(isMobile && { paddingLeft: undefined }),
         }}>
-            <div style={{ textAlign: "center", width: "21vw" }}>
-                <h2>Activating Elabox</h2>
-                <p>Unlock rewards and premium support</p>
-                <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-                    <Button style={{ padding: 10, width: 100 }} size="sm">Skip</Button>
-                    <Button style={{ padding: 10, width: 100 }} color="success" size="sm" ><span>Activate Now</span></Button>
-                </div>
+            <div style={{ textAlign: "center", width: `${isMobile ? "80vw" : "40vw"}` }}>
+                {currentPage === 0 ? <ActivatePage handleNextPage={handleNextPage} /> : <PurchasePage handlePrevPage={handlePrevPage} />}
             </div>
+
         </div >
     )
+}
+const ActivatePage = ({ handleNextPage }) => {
+    return <>
+        <h2>Activating Elabox</h2>
+        <p>Unlock rewards and premium support</p>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <Link to="/">
+                <Button style={{ padding: 10, width: 100 }} size="sm">Skip</Button>
+            </Link>
+            <Button style={{ padding: 10, width: 100 }} color="success" size="sm" onClick={handleNextPage} >Activate Now</Button>
+        </div>
+    </>
+}
+const PurchasePage = ({ handlePrevPage }) => {
+    return <>
+        <Table style={{ color: "white" }} size="sm" borderless >
+            <thead>
+                <th></th>
+                <th>Free</th>
+                <th>Premium</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style={{ paddingRight: 30 }}>Nodes(Mainchain,ESC,DID)</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                </tr>
+                <tr>
+                    <td>dApps</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                </tr>
+                <tr>
+                    <td>dApps services</td>
+                    <td>{<Icon.XCircle height={20} width={20} color="red" />}</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                </tr>
+                <tr>
+                    <td>Premium Support</td>
+                    <td>{<Icon.XCircle height={20} width={20} color="red" />}</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                </tr>
+                <tr>
+                    <td>Rewards</td>
+                    <td>{<Icon.XCircle height={20} width={20} color="red" />}</td>
+                    <td>{<Icon.CheckCircle height={20} width={20} color="green" />}</td>
+                </tr>
+            </tbody>
+        </Table>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: 100 }}>
+            <div style={{ display: "inline-block", cursor: "pointer" }} onClick={() => handlePrevPage()}> <Icon.ArrowLeft /> Back</div>
+            <div>
+                <Button style={{ padding: 10, width: 100, marginRight: 5 }} size="sm" onClick={() => { handlePrevPage("/") }}>Later</Button>
+                <Button style={{ padding: 10, width: 100 }} color="success" size="sm">Activate Now</Button>
+            </div>
+
+        </div>
+    </>
 }
