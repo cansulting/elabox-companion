7ed751db13c802fe853d5656c31f12d3f8279807
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-03-23 10:28:46 +0000

    added rpc backup for getbalance

--- src_server/index.js
+++ src_server/index.js
@@ -332,17 +332,34 @@ router.post("/getBalance", (req, res) => {
     "curl http://localhost:20334/api/v1/asset/balances/" + address,
     { maxBuffer: 1024 * maxBufferSize },
     async (err, stdout, stderr) => {
+      let balance = "...";                    
       if (err)
-        syslog.write(
-          syslog
-            .create()
-            .error("Error retrieving wallet balance", err)
-            .addCaller()
-        );
-      let balance = "...";
-      if (stdout !== "") {
-        let balanceInfo = JSON.parse(stdout);
-        balance = balanceInfo.Result;
+      {
+        const data={
+          "method":"getreceivedbyaddress",
+          "params":{"address": "EZuYxNHK3ZSRARh1L5RMNwPucx1xiJ6mSo"}
+        }
+        const headers={
+          "Content-Type":"application/json"
+        }
+         await axios.post(config.ELA_RPC_URL,data,{headers}).then(response=>{
+          const { data } = response
+          balance=parseFloat(data.result)
+          res.json({balance})
+        }).catch(err=>{
+          syslog.write(
+            syslog
+              .create()
+              .error("Error retrieving wallet balance", err)
+              .addCaller()
+          );
+        })
+      }
+      else{
+        if (stdout !== "") {
+          let balanceInfo = JSON.parse(stdout);
+          balance = balanceInfo.Result;
+        }        
       }
       res.json({ balance });
     }
