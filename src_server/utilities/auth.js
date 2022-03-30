384c9e0b18a9de676e8e15c35f3aa8fa52d93ec2
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-03-30 17:03:33 +0000

    remove console.log

--- src_server/utilities/auth.js
+++ src_server/utilities/auth.js
@@ -191,7 +191,6 @@ const  generateAccessToken = () =>{
 const authenticateToken = (req, res, next)=> {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    console.log(token)
     if (token == null) return res.sendStatus(401)
   
     jwt.verify(token, config.AUTH_TOKEN, (err, username) => {
