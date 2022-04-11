8905d35af36a82a09696bc3482022320131e3bea
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-04-11 23:43:20 +0000

    added restart and resync

---
+++ src_client/src/dapp-store/RestartModal.jsx
@@ -0,0 +1,101 @@
+import React,{useState} from "react";
+
+import { Button,  Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader ,Input} from "reactstrap"
+import backend from "../api/backend"
+import errorLogo from "../views/images/error.png";
+const RestartModal = ({isOpen,name,closeModal, node })=> {
+    const [password , setPassword] = useState("")
+    const [errorModal,setErrorModal] = useState(false)
+    const hamdleChangePassword = (event) => {
+        const { target } = event;
+        const { name } = target;
+        setPassword(target.value)
+      } 
+      const verifyPassword = (action) => {
+        // e.preventDefault();
+        closeModal(false) 
+        backend
+        .authentication(password)
+        .then((responseJson) => {
+          if (responseJson.ok) {
+            restartNode(password,name)
+          }
+          else{
+            setErrorModal(true)
+          }
+
+        })        
+    }
+    const restartNode = (pwd) => {
+        // e.preventDefault();
+        closeModal()    
+        node
+          .restart(pwd)
+          .then((responseJson) => {
+            node.fetchData();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };    
+      const errorToggle= () =>{
+        setErrorModal(false)
+      }
+    return  <>
+            <Modal isOpen={errorModal} centered>
+                <ModalHeader>Error</ModalHeader>
+                <ModalBody>
+                    <center>
+                    Invalid password, please try again
+                    <br />
+                    <br />
+                    <img src={errorLogo} style={{ width: "50px", height: "50px" }} />
+                    </center>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={errorToggle}>
+                    Close
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            <Modal isOpen={isOpen} centered>
+            <ModalHeader> Restart {name}</ModalHeader>
+            <ModalBody>
+            <center>
+                You are about to restart the {name}
+                <br />
+                This process will take a few hours
+                <br />
+                <br />              
+                <Input
+                type="password"
+                id="pwd"
+                name="pwd"
+                placeholder="Enter ELA wallet password"
+                required
+                onChange={hamdleChangePassword}
+                />              
+                <br />
+                <br />              
+            </center>
+            </ModalBody>
+            <ModalFooter>
+            <Button
+                data-testid="restart-btn"
+                color="success"
+                onClick={verifyPassword}              
+            >
+                Restart
+            </Button>
+            <Button color="danger" onClick={closeModal}>
+                Cancel
+            </Button>
+            </ModalFooter>
+            </Modal>      
+    </>      
+}
+
+export default RestartModal
---
+++ src_client/src/dapp-store/ResyncModal.jsx
@@ -0,0 +1,124 @@
+import React,{useState} from "react";
+
+import { Button,  Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader ,Input} from "reactstrap"
+import backend from "../api/backend"
+import errorLogo from "../views/images/error.png";
+import checkLogo from "../views/images/check.png";
+const ResyncModal = ({isOpen,name,closeModal, node })=> {
+    const [password , setPassword] = useState("")
+    const [resyncSuccessModal,setResyncSuccessModal] = useState(false)
+    const [errorModal,setErrorModal] = useState(false)
+    const hamdleChangePassword = (event) => {
+        const { target } = event;
+        const { name } = target;
+        setPassword(target.value)
+      } 
+      const verifyPassword = (action) => {
+        // e.preventDefault();
+        closeModal(false) 
+        backend
+        .authentication(password)
+        .then((responseJson) => {
+          if (responseJson.ok) {
+            setResyncSuccessModal(true)
+            resyncNode(password,name)
+          }
+          else{
+            setErrorModal(true)
+          }
+
+        })        
+    }
+    const resyncNode = (pwd) => {
+        // e.preventDefault();
+        closeModal()    
+        node
+          .resync(pwd)
+          .then((responseJson) => {
+            node.fetchData();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };    
+      const errorToggle= () =>{
+        setErrorModal(false)
+      }
+      const resyncSuccessToggle = ()=>{
+        setResyncSuccessModal(false)
+      }
+    return  <>
+            <Modal isOpen={errorModal} centered>
+                <ModalHeader>Error</ModalHeader>
+                <ModalBody>
+                    <center>
+                    Invalid password, please try again
+                    <br />
+                    <br />
+                    <img src={errorLogo} style={{ width: "50px", height: "50px" }} />
+                    </center>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={errorToggle}>
+                    Close
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            <Modal isOpen={resyncSuccessModal} centered>
+                <ModalHeader>Success</ModalHeader>
+                <ModalBody>
+                    <center>
+                    Resyncing Node <br />
+                    <br />
+                    <img src={checkLogo} style={{ width: "50px", height: "50px" }} />
+                    </center>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={resyncSuccessToggle}>
+                    Close
+                    </Button>
+                </ModalFooter>
+            </Modal>            
+            <Modal isOpen={isOpen} centered>
+            <ModalHeader> Resync {name}</ModalHeader>
+            <ModalBody>
+            <center>
+              <b>PLEASE READ CAREFULY</b>
+              <br />
+              Resycing the will take a few days.
+              <br />
+              You should try to restart the node first!
+              <br />
+              <br />
+              Enter your wallet password to re-sync the {name}
+              <br />
+            </center>
+            <Input
+              type="password"
+              id="pwd"
+              name="pwd"
+              placeholder="Enter ELA wallet password"
+              required
+              onChange={hamdleChangePassword}
+            />
+            </ModalBody>
+            <ModalFooter>
+            <Button
+                data-testid="resync-btn"
+                color="success"
+                onClick={verifyPassword}              
+            >
+                Resync
+            </Button>
+            <Button color="danger" onClick={closeModal}>
+                Cancel
+            </Button>
+            </ModalFooter>
+            </Modal>      
+    </>      
+}
+
+export default ResyncModal
