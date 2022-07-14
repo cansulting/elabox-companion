38460651bac48c69840819cda77f47a2c9a64e35
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-07-14 12:20:00 +0000

    update handling

--- src_client/src/App.js
+++ src_client/src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
         <Router basename={process.env.PUBLIC_URL}>
           <div>
           <Helmet>
-              {isOnionUrl && <>
+              {!isOnionUrl && <>
                   <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
                   <meta http-equiv="Pragma" content="no-cache" />
                   <meta http-equiv="Expires" content="0" />                
